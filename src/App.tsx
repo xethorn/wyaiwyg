58e0789faba967e59ccf42c292b05045ec1086fa
Michael Ortali <github@xethorn.net>
2026-07-18 12:50:01 -0400

    Change folder SVG to dynamically look open/closed depending on project expansion state

--- src/App.tsx
+++ src/App.tsx
@@ -301,6 +301,7 @@ function App() {
               </svg>
               <svg className="menu-item-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ width: "14px", height: "14px" }}>
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+                {projectExpanded && <line x1="2" y1="10" x2="22" y2="10"></line>}
               </svg>
               <span>wyaiwyg (xethorn/wyaiwyg)</span>
             </div>
